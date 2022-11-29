1fe2fa9298d7b62330de53f5ddcbc09bd0036e73
Cynthia Uche <cynthianuchepro@gmail.com>
2022-11-28 20:21:27 -0800

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: the singly linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *checkhead, *nexthead;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	checkhead = list;
+	nexthead = list->next;
+
+	while (nexthead && nexthead->next)
+	{
+		if (checkhead == nexthead)
+			return (1);
+		nexthead = nexthead->next->next;
+		checkhead = checkhead->next;
+	}
+
+	return (0);
+}
