470c609e7a401ef506ed1155433ea3a171a1119c
Cynthia Uche <cynthianuchepro@gmail.com>
2022-11-29 14:16:53 -0800

    Create 6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for n in range(0, 10):
+        if n != i and n > i:
+            if i == 8 and n == 9:
+                print("{}{}".format(i, n))
+            else:
+                print("{}{}, ".format(i, n), end="")
