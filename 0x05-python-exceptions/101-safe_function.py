cbbf2a367f9130f1aa8e82f8bd62ff24b6243f6b
Cynthia Uche <cynthianuchepro@gmail.com>
2022-12-28 15:22:55 -0800

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+
+    except BaseException as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
