a1053e611adcaa8e049fde786b6be2faf0373bca
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-10 03:51:20 -0800

    Create 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This function contains a class 'is_same_class'"""
+
+
+def is_same_class(obj, a_class):
+    """Returns `True` if the object is exactly an instance of the specified class
+    ; otherwise `False`
+    """
+    return type(obj) is a_class
