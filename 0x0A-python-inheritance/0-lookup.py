4637c2c6c7565bed45d69d76036cbb0036ed8291
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-10 03:29:52 -0800

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function 'lookup'."""
+
+
+def lookup(obj):
+    """This function returns the list of available attributes and
+    methods of an object.
+    """
+    return dir(obj)
