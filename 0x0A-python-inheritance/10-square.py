5f0571e067fb8d50d3771274cb5b22931b7ad151
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-15 22:57:09 -0800

    Create 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a class ``Square``"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This geometry class ``Square`` inherits from ``Rectangle``"""
+    
+    def __init__(self, size):
+        """This method initializes the class"""
+        self.integer_validator('size', size)
+        super().__init__(size, size)
+
