e4d148662468c4ef302b7c4089f9abae2a261092
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-15 22:59:59 -0800

    Fix pycodestyle fail for10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -5,9 +5,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """This geometry class ``Square`` inherits from ``Rectangle``"""
-    
+
     def __init__(self, size):
         """This method initializes the class"""
         self.integer_validator('size', size)
         super().__init__(size, size)
-
