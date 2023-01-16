e61b72cea44f340973f8a7137a0de390620116da
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-16 06:30:27 -0800

    Update 102-magic_calculation.py'

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 def magic_calculation(a, b):
     result = 0
-    
+
     for i in range(1, 3):
         try:
             if i > a:
@@ -12,4 +12,3 @@ def magic_calculation(a, b):
         except Exception:
             result = b + a
     return result
-
