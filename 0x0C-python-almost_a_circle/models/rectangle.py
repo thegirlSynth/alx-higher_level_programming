3ad2bc617684cdc7ca781c61726e0e54e596d002
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-17 10:27:41 -0800

    Create str method for Rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,9 @@ class Rectangle(Base):
             for k in range(0, self.__width):
                 print('#', end="")
             print()
+
+    def __str__(self):
+        """String method for Rectangle class"""
+        first_part = ' ({}) {}/{}'.format(self.id, self.__x, self.__y)
+        second_part = ' - {}/{}'.format(self.__width, self.__height)
+        return '[Rectangle]' + first_part + second_part
