892861994b36b15ffd6dbaae7e0767b7f4d1d58c
Cynthia Uche <cynthianuchepro@gmail.com>
2023-01-29 23:40:31 -0800

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class Square"""
+
+
+class Square:
+    """This is a geometry class"""
+    def __init__(self, size):
+        """Instantiates an Instance of Square"""
+        self.__size = size
