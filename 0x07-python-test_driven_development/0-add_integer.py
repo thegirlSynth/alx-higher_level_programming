99664afd679af05b94eee2e2c39cda3e20eea4ad
Cynthia Uche <cynthianuchepro@gmail.com>
2023-02-05 10:16:24 -0800

    Update documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""This module contains the function `add_integer`"""
 
 
 def add_integer(a, b=98):
+    """This function adds two integers.
+
+    Args:
+        a(int/float) - the first integer
+        b(int/float) - the second integer
+
+    Returns:
+        the sum of a and b, raises an exception otherwise
+    """
     if type(a) is float:
         a = int(a)
     if type(b) is float:
