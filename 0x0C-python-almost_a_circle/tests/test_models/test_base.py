532a865d6e53b33064af673cc9877af107ce2927
Cynthia Uche <cynthianuchepro@gmail.com>
2023-02-06 08:40:59 -0800

    Test Base class instantiation

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,15 +2,15 @@ import unittest
 from models.base import Base
 
 
-class BaseModelTestCase(unittest.TestCase):
-    def test_instantiation(self):
+class Test_instantiation(unittest.TestCase):
+    def test_no_arg(self):
         b1 = Base()
         b2 = Base()
         self.assertNotEqual(b1, b2)
 
     def test_id_is_none(self):
-        b1 = Base()
-        b2 = Base()
+        b1 = Base(None)
+        b2 = Base(None)
         self.assertEqual(b1.id, b2.id-1)
 
     def test_id_not_none(self):
@@ -20,8 +20,10 @@ class BaseModelTestCase(unittest.TestCase):
     def test_instance_after_unique_id(self):
         b1 = Base()
         b2 = Base()
-        b4= Base(None)
+        b3 = Base(30)
+        b4 = Base(None)
         self.assertEqual(b4.id-1, b2.id)
+
 
 if __name__ == '__main__':
     unittest.main()
