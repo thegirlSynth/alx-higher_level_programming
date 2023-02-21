af0c1ebb87838bf446c4f3e0c49e5adfd3b3d44a
Cynthia Uche <cynthianuchepro@gmail.com>
2023-02-21 22:55:51 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates  the table states on the database hbtn_0d_usa 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`);
+    PRIMARY KEY(`id`),
     `id` INT    NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(256) NOT NULL
 );
