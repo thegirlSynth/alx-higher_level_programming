213e4bb8d94b4f0bd782ce8b9359ead1b62106ee
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-14 13:24:50 -0700

    Solve Task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,11 @@
+#/usr/bin/node
+"""This module contains a function `print_square`"""
+
+def print_square(size):
+    """This function prints a square with the character `#`"""
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(0, size):
+        print('#' * size)
