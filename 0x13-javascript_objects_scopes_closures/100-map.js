15372edaae6c28960553b89dd5563b58dbe5b179
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-17 05:59:36 -0700

    Fix failing checks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-list = require('./100-data').list;
-newList = list.map((num) => num * list.indexOf(num));
+const list = require('./100-data').list;
+const newList = list.map((num, index) => num * index);
 console.log(list);
 console.log(newList);
