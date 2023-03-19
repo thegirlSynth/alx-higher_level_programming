b7d2197ac3950704392a03d2506f8180ee5999a9
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-18 17:42:06 -0700

    Create 11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record displays: tv_shows.title - tv_show_genres.genre_id
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- If a show doesn’t have a genre, NULL is displayed
+-- Only one SELECT statement is used
+
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_shows`
+LEFT JOIN `tv_show_genres` ON `tv_shows`.`id`=`tv_show_genres`.`show_id`
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
