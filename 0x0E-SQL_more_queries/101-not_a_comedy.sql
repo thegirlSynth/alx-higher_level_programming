5122e627ff7cb071eae58efd4cd73f6aa288f336
Cynthia Uche <cynthianuchepro@gmail.com>
2023-03-27 09:17:58 -0700

    Create 101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,16 @@
+-- Lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
+-- You can use a maximum of two SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT `tv_shows`.`title`
+FROM `tv_shows`
+WHERE `tv_shows`.`id` NOT IN (
+	SELECT `tv_show_genres`.`show_id`
+	FROM `tv_show_genres`
+	JOIN `tv_genres` ON `tv_genres`.`id`=`tv_show_genres`.`genre_id`
+	WHERE `tv_genres`.`name`="Comedy"
+)
+ORDER BY `tv_shows`.`title`;
