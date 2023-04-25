feab2a12eeb023977ae073091e66325e05b55493
Cynthia Uche <61497064+thegirlSynth@users.noreply.github.com>
2023-04-25 18:14:06 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ request(process.argv[2], (error, response, body) => {
     console.error(error);
     return;
   }
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
