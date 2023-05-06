c54679429607e00009ae52d742393ce2a1cb4929
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-06 05:26:56 -0700

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | awk '/content-length/ { print $2 }'
+curl -sI "$I" | awk '/content-length/ { print $2 }'
