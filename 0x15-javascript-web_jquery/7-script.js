4d9de6f0ea7c392401f37d5e050dfba619f33428
Cynthia Uche <cynthianuchepro@gmail.com>
2023-05-31 23:07:31 +0100

    Solve tasks 4 to 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "https://swapi-api.alx-tools.com/api/people/5/?format=json",
+        dataType: "json",
+        success: function(data) {
+            $("#character").text(data.name);
+        }
+    });
+});
